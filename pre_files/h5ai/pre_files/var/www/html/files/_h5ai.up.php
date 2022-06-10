2adf0cd8efd96291b8eaac2cc48a3678880e6abe
teasiu <teasiu@qq.com>
2022-06-10 17:04:00 +0800

    修复上传插件乱码

--- pre_files/h5ai/pre_files/var/www/html/files/_h5ai.up.php
+++ pre_files/h5ai/pre_files/var/www/html/files/_h5ai.up.php
@@ -8,7 +8,7 @@
     // 定义存放上传文件的真实路径名字 
     $name = $_FILES['userfile']['name']; 
     // 将文件的名字的字符编码从UTF-8转成GB2312 
-    $name = iconv("UTF-8", "GBK", $name); 
+    // $name = iconv("UTF-8", "GBK", $name); 
     // 将上传文件移动到指定目录文件中 
     if (move_uploaded_file($_FILES['userfile']['tmp_name'], $path.$name)) { 
       echo "文件上传成功 </br>";
