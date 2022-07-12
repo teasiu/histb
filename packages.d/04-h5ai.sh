b72abb93a7048804569ee7cbb036b3bb0c8eb9d1
teasiu <teasiu@qq.com>
2022-07-12 09:39:49 +0800

    fix erro

--- packages.d/04-h5ai.sh
+++ packages.d/04-h5ai.sh
@@ -8,6 +8,6 @@ cd ${WORK_PATH}
 mkdir -p ${WWW_PATH}/files
 mkdir -p ${ROOTFS}/home/ubuntu/files
 chmod -R 777 ${ROOTFS}/home/ubuntu/files
-ln -sf ${ROOTFS}/home/ubuntu/files ${WWW_PATH}/files/home
+ln -sf /home/ubuntu/files ${WWW_PATH}/files/home
 
 tar -zxvf package_files/h5ai/h5ai.tar.gz -C ${WWW_PATH}/files > /dev/null 2>&1
