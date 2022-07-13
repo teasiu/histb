e71081a2b0e1ca185a7ba1bf0e020f3d28a03514
teasiu <teasiu@qq.com>
2022-07-14 02:34:41 +0800

    vlmcsd permission

--- packages.d/16-vlmcsd.sh
+++ packages.d/16-vlmcsd.sh
@@ -1,4 +1,5 @@
 # install vlmcsd
 
 chmod a+x ${ROOTFS}/usr/bin/vlmcsd
+chmod 644 ${ROOTFS}/etc/systemd/system/vlmcsd.service
 ln -sf ${ROOTFS}/etc/systemd/system/vlmcsd.service ${ROOTFS}/etc/systemd/system/multi-user.target.wants/vlmcsd.service
