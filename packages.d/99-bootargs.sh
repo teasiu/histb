8fa18fbdc78054b8200a1448b8fe50564e67cb0a
teasiu <teasiu@qq.com>
2022-07-14 13:51:43 +0800

    add bootargs.txt

--- packages.d/99-bootargs.sh
+++ packages.d/99-bootargs.sh
@@ -47,6 +47,7 @@ cd ${WORK_PATH}
 echo "hi3798$bootargs" > target
 [ "$ARCH" = "arm64" ] && is64="-64"
 cp -a package_files/bootargs/bootargs4-${bootargs}${is64}.bin ${ROOTFS}/usr/bin/bootargs4.bin
+cp -a package_files/bootargs/emmc_bootargs-${bootargs}${is64}.txt ${ROOTFS}/etc/emmc_bootargs_input.txt
 cp -a package_files/bootargs/recoverbackup_${ARCH} ${ROOTFS}/usr/bin/recoverbackup
 chmod 777 ${ROOTFS}/usr/bin/recoverbackup
 echo "hi3798$bootargs" > ${ROOTFS}/etc/hostname
