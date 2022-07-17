e61a67519cdd70dd698e1c3261b8a8f5351c89d1
teasiu <teasiu@qq.com>
2022-07-17 17:36:38 +0800

    使用新分区表

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nothing test
+旧版分区表请使用git checkout -b 20220716
--- package_files/frpc/pre_files/common/etc/first_init.d/frpc.sh
+++ package_files/frpc/pre_files/common/etc/first_init.d/frpc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 subdomainid=$(awk 'NR==1' /etc/first_init 2> /dev/null)
-sshport=`shuf -i 10000-65535 -n1`
+sshport=`shuf -i 10000-30000 -n1`
 sed -i "s/xxxxx/$subdomainid/g" /etc/frp/frpc.ini
 sed -i "s/ppppp/$sshport/g" /etc/frp/frpc.ini
