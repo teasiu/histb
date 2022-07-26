05297cb110ce5264ff8317416eed4d1986343d4c
Hyy2001X <1804430051@qq.com>
2022-07-26 21:43:52 +0800

    profile.d: improve code

--- package_files/others/pre_files/common/etc/profile.d/99-helloworld.sh
+++ package_files/others/pre_files/common/etc/profile.d/99-helloworld.sh
@@ -1,6 +1,6 @@
 IP=$(ifconfig eth0 | grep '\<inet\>'| grep -v '127.0.0.1' | awk '{print $2}' | awk 'NR==1')
-TARGET=$(dmesg 2> /dev/null | grep "CPU: hi" | awk -F ':[ ]' '/CPU/{printf ($2)}')
-[ ! "$TARGET" ] && TARGET=$(head -n 1 /etc/regname 2> /null)
+DEVICE=$(dmesg 2> /dev/null | grep "CPU: hi3798" | awk -F ':[ ]' '/CPU/{printf ($2)}')
+[ ! "$DEVICE" ] && DEVICE=$(head -n 1 /etc/regname 2> /null)
 clear
 echo -e "\e[33m
       __  ____                __       
@@ -9,12 +9,13 @@ echo -e "\e[33m
    / /_/ / /_/ / /_/ / / / / /_/ /_/ / 
    \____/_.___/\__,_/_/ /_/\__/\__,_/ 
 \e[0m
-		欢迎使用 \e[31m海思机顶盒微型 NAS 服务器 $(getconf LONG_BIT)-bit 系统\e[0m
-		原创作者 神雕Teasiu 贡献者 Hyy2001 MinaDee Xjm
-		浏览详细使用教程请访问: \e[32mhttp://$IP\e[0m
-		访问我们的官网: \e[32mhttps://www.histb.com\e[0m
+	  欢迎使用 \e[31m海思机顶盒微型 NAS 服务器 $(getconf LONG_BIT)-bit 系统\e[0m
+	  原创作者: 神雕Teasiu
+	  贡献者: Hyy2001 MinaDee Xjm
+	  浏览详细使用教程请访问: \e[32mhttp://$IP\e[0m
+	  访问我们的官网: \e[32mhttps://www.histb.com\e[0m
 
-   设备名称 : $TARGET
+   设备名称 : ${DEVICE}_$(egrep -oa "hi3798.+reg" /dev/mmcblk0p1 2> /dev/null | cut -d '_' -f1)
    系统版本 : $(awk -F '[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release) | V$(cat /etc/nasversion)-$(uname -r)-$(getconf LONG_BIT)
    可用存储 : $(df -m / | grep -v File | awk '{a=$4*100/$2;b=$4} {printf("%.1f%s %.1fM\n",a,"%",b)}')
    可用内存 : $(free -m | grep Mem | awk '{a=$7*100/$2;b=$7} {printf("%.1f%s %.1fM\n",a,"%",b)}') | $(free -m | grep Swap | awk '{a=$4*100/$2;b=$4} {printf("%.1f%s %.1fM\n",a,"%",b)}')
@@ -28,3 +29,4 @@ alias ramfree='sync && echo 3 > /proc/sys/vm/drop_caches'
 alias cls='clear'
 alias syslog='cat /var/log/syslog'
 alias unmount='umount -l'
+alias reg="egrep -oa 'hi3798.+' /dev/mmcblk0p1 | awk '{print $1}'"
