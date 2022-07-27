7078d71817cb5060246cfe08e3cc4843fabeaba7
teasiu <teasiu@qq.com>
2022-07-28 02:22:07 +0800

    add checkone

--- packages.d/99-bootargs.sh
+++ packages.d/99-bootargs.sh
@@ -10,5 +10,7 @@ cp -a package_files/bootargs/bootargs7-${bootargs}${is64}.bin ${ROOTFS}/usr/bin/
 cp -a package_files/bootargs/emmc_bootargs-${bootargs}${is64}.txt ${ROOTFS}/etc/bootargs_input.txt
 chmod 777 ${ROOTFS}/etc/bootargs_input.txt
 cp -a package_files/bootargs/recoverbackup_${ARCH} ${ROOTFS}/usr/bin/recoverbackup
+cp -a package_files/bootargs/checkone_${ARCH} ${ROOTFS}/usr/bin/checkone
 chmod 777 ${ROOTFS}/usr/bin/recoverbackup
+chmod 755 ${ROOTFS}/usr/bin/checkone
 echo "hi3798$bootargs" > ${ROOTFS}/etc/hostname
