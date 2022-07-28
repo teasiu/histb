9201c432c24cacf1d123e105d43a8cca96b5122f
xjm1285 <xjm1285@gmail.com>
2022-07-28 14:02:25 +0000

    fix vlmcsd service link

--- packages.d/16-vlmcsd.sh
+++ packages.d/16-vlmcsd.sh
@@ -2,4 +2,4 @@
 
 chmod a+x ${ROOTFS}/usr/bin/vlmcsd
 chmod 644 ${ROOTFS}/etc/systemd/system/vlmcsd.service
-ln -sf ${ROOTFS}/etc/systemd/system/vlmcsd.service ${ROOTFS}/etc/systemd/system/multi-user.target.wants/vlmcsd.service
+ln -sf /etc/systemd/system/vlmcsd.service ${ROOTFS}/etc/systemd/system/multi-user.target.wants/vlmcsd.service
