fe0ec34fc5c33ba22089f22f185990f5921230ad
xjm1285 <xjm1285@gmail.com>
2022-07-31 12:19:22 +0000

    rename install-tailscale.sh to install-tailscale

--- packages.d/22-tailscale.sh
+++ packages.d/22-tailscale.sh
@@ -3,6 +3,6 @@
 install_dir="/opt/tailscale"
 
 chmod a+x ${ROOTFS}/$install_dir/tailscale*
-chmod a+x ${ROOTFS}/usr/bin/install-tailscale.sh
+chmod a+x ${ROOTFS}/usr/bin/install-tailscale
 ln -sf $install_dir/tailscale ${ROOTFS}/usr/bin/tailscale
 ln -sf $install_dir/tailscaled ${ROOTFS}/usr/bin/tailscaled
