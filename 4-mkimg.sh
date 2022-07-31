4114f1c24b0e3e2b61d3ee5324538cb46c93c5e8
xjm1285 <xjm1285@gmail.com>
2022-07-31 10:50:16 +0000

    change backup name and reduce img size

--- 4-mkimg.sh
+++ 4-mkimg.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 timestamp=$(date +%Y%m%d)
-file_prefix="Ubuntu-$(awk 'NR==1' target_arch 2> /dev/null)-$(awk 'NR==2' target_arch 2> /dev/null)-hi3798$(awk 'NR==1' target 2> /dev/null)"
+model=$(awk 'NR==1' target 2> /dev/null)
+ARCH=$(awk 'NR==2' target_arch 2> /dev/null)
+if [ "$ARCH" = "arm64" ]; then
+    arch_suffix="-64"
+else
+    arch_suffix="-32"
+fi
+file_prefix="Ubuntu-$(awk 'NR==1' target_arch 2> /dev/null)-${ARCH}-hi3798${model}"
 img_file="${file_prefix}-${timestamp}-MD5.img"
 TMPFS="tmp"
 
 echo "Firmware Model: $img_file"
 echo "Firmware will output into: $(pwd), Please wait ..."
 
-dd if=/dev/zero of=$img_file count=1000 obs=1 seek=1280M
+dd if=/dev/zero of=$img_file count=1000 obs=1 seek=900M
 mkfs.ext4 $img_file
 umount $TMPFS 2> /dev/null ; rm -r "$TMPFS" 2> /dev/null ; mkdir -p $TMPFS ; sleep 1
 mount $img_file $TMPFS
@@ -36,10 +43,10 @@ echo "Packed Firmware Size: $(du -h $gz_file 2> /dev/null | awk '{print $1}')"
 
 backup_img="${file_prefix}-${timestamp}-backup-MD5.img"
 dd if=/dev/zero of=$backup_img count=1 obs=1 seek=536870400
-mkfs.ext4 $backup_img
+mkfs.ext2 $backup_img
 umount $TMPFS 2> /dev/null ; rm -r "$TMPFS" 2> /dev/null ; mkdir -p $TMPFS ; sleep 1
 mount $backup_img $TMPFS
-cp $gz_file $TMPFS
+cp $gz_file $TMPFS/backup-${model}${arch_suffix}.gz
 sync
 umount $TMPFS 2> /dev/null ; rm -r "$TMPFS" 2> /dev/null
 e2fsck -p -f $backup_img
