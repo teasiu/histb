944e418d744301c169b36f4aaf00c9fb96caad19
teasiu <teasiu@qq.com>
2022-08-01 20:20:26 +0800

    fix frpc for web

--- package_files/web/pre_files/common/etc/first_init.d/web.sh
+++ package_files/web/pre_files/common/etc/first_init.d/web.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+if [ -f /usr/bin/frpc ];then 
 subdomainid=$(awk 'NR==1' /etc/first_init 2> /dev/null)
 sed -i "s/xxxxx/$subdomainid/g" /var/www/html/index2.html
 sed -i "s/xxxxx/$subdomainid/g" /var/www/html/zhinan.html
-
+fi
